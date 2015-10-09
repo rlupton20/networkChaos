8889de8426136d266d4805a9b0b7e31444b1c23f
Richard <richard.lupton@gmail.com>
2015-10-09 07:53:57 +0100

    Added a command line option for selecting an interface. Also displays a message to say its listening, and gives the interface. Now set to display the parsed headers.

--- packetCollector.hs
+++ packetCollector.hs
@@ -1,16 +1,13 @@
+import System.Environment
+
 import Network.Pcap
+
 import Net.Packet
 import Net.PacketParsing
 import qualified  Net.Ethernet
 import qualified Net.IPv4
-import qualified Data.ByteString as B
 
--- Any network interface can be put in here. rvl0 is a dummy network
--- we create using ip link in a script. Other interfaces can be used to
--- check functionality.
-interface :: String
---interface = "rvl0"
-interface = "enp0s18f2u1"
+import qualified Data.ByteString as B
 
 formatForParsing :: B.ByteString -> InPacket
 formatForParsing p = toInPack $ listArray (0, B.length p - 1) (B.unpack p)
@@ -30,8 +27,12 @@ unparseTest pbs = do
 
 main :: IO ()
 main = do
+  args <- getArgs
+  let interface = if null args then "rvl0" else (args!!0)
+  
   device <- openLive interface 0xFFFF False 0
   setFilter device "ether proto 0x0800" True 0
-  
-  loopBS device (-1) (\_ packet -> putStrLn.show $ unparseTest packet)
+
+  putStrLn $ "Listening on " ++ interface
+  loopBS device (-1) (\_ packet -> putStrLn.show $ parseIP4 packet)
   return ()
