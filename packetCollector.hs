2df6d07f21fa2e953a0846b0a124c7b649ed6b2f
Richard <richard.lupton@gmail.com>
2015-10-09 07:02:13 +0100

    Started playing with unparsing. Found a little incompleteness in the library, but still managed to get some code to work. Added a little check that parses and unparses, and checks we get back what we put in (at least, as an OutPacket rather than an InPacket).

--- packetCollector.hs
+++ packetCollector.hs
@@ -1,16 +1,16 @@
 import Network.Pcap
 import Net.Packet
 import Net.PacketParsing
-import Net.Ethernet
-import Net.IPv4
+import qualified  Net.Ethernet
+import qualified Net.IPv4
 import qualified Data.ByteString as B
 
 -- Any network interface can be put in here. rvl0 is a dummy network
 -- we create using ip link in a script. Other interfaces can be used to
 -- check functionality.
 interface :: String
-interface = "rvl0"
---interface = "enp0s18f2u1"
+--interface = "rvl0"
+interface = "enp0s18f2u1"
 
 formatForParsing :: B.ByteString -> InPacket
 formatForParsing p = toInPack $ listArray (0, B.length p - 1) (B.unpack p)
@@ -18,10 +18,20 @@ formatForParsing p = toInPack $ listArray (0, B.length p - 1) (B.unpack p)
 parseIP4 :: B.ByteString -> Maybe (Net.Ethernet.Packet (Net.IPv4.Packet InPacket))
 parseIP4 = doParse . formatForParsing
 
+-- This checks that packets once parsed, unparse to the same thing.
+-- Note how the interface to Net.Ethernet.Packet is incomplete.
+unparseTest :: B.ByteString -> Maybe Bool
+unparseTest pbs = do
+                  let p = formatForParsing pbs
+                  pck <- parseIP4 pbs
+                  let pu = loopbackout p
+                      pcku = Net.Ethernet.unparse $ fmap doUnparse pck
+                  return (outBytes pu == outBytes pcku)
+
 main :: IO ()
 main = do
   device <- openLive interface 0xFFFF False 0
   setFilter device "ether proto 0x0800" True 0
   
-  loopBS device (-1) (\_ packet -> putStrLn.show $ parseIP4 packet)
+  loopBS device (-1) (\_ packet -> putStrLn.show $ unparseTest packet)
   return ()
