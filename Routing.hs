2c4cd3e64bd61ab5e9dfd893fa86591c93681891
Richard <richard.lupton@gmail.com>
2015-10-16 15:19:36 +0100

    Started factoring the routing code into more manageable chunks. Encapsulated the raw socket in a function called dispatchTo.

--- Routing.hs
+++ Routing.hs
@@ -18,6 +18,19 @@ routing = (Net.IPv4.Addr 10 0 0 10, Net.IPv4.Addr 192 168 0 104)
 addrToString :: Net.IPv4.Addr -> String
 addrToString (Net.IPv4.Addr s1 s2 s3 s4) = init.concat.map (++".") $ map show [s1, s2, s3, s4]
 
+dispatchTo :: B.ByteString -> String -> IO Int
+dispatchTo packet address = do
+  ads <- getAddrInfo Nothing (Just address) Nothing
+  trySendingTo ads
+  where trySendingTo ads = case ads of [] -> return 0
+                                       (ad:adss) -> do
+                                         let sockAddr = addrAddress ad
+                                         sock <- socket AF_INET Raw 255
+                                         connect sock sockAddr
+                                         sent <- send sock packet
+                                         close sock
+                                         if sent == 0 then trySendingTo adss else return sent
+                                                     
 processIP4 :: Net.IPv4.Packet InPacket -> IO ()
 processIP4 packet = withSocketsDo $ do
   
@@ -28,12 +41,7 @@ processIP4 packet = withSocketsDo $ do
 
   putStrLn $ (show destination) ++ " --> " ++ (show $ Net.IPv4.dest routedPacket)
 
-  ad <- getAddrInfo Nothing (Just $ addrToString newdest) Nothing
-  let addr = addrAddress.head $ ad
-
-  socket <- socket AF_INET Raw 255  
-  connect socket addr
-  sent <- send socket toSend
+  sent <- toSend `dispatchTo` (addrToString newdest)
   
   putStrLn $ show sent ++ " bytes sent"
 
