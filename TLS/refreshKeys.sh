45d7467b53fbd0091d60956a0ce1361641803a67
Richard <richard.lupton@gmail.com>
2015-10-30 15:35:15 +0000

    Figured out how to load keys and certificates using credentialLoadX509. This also required keys to be generated without passwords, so refreshKeys was adapted to make this work. Notes also updated, as were keys.

--- TLS/refreshKeys.sh
+++ TLS/refreshKeys.sh
@@ -2,4 +2,4 @@
 
 rm cert.pem
 rm key.pem
-openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 30
+openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 30 -nodes
