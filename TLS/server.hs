caf0dd5f75bd52fca08e6ac88fef3c56903d5eee
Richard <richard.lupton@gmail.com>
2015-11-02 16:32:05 +0000

    Got a working TLS server. Needs some tidying, testing, and maybe made to use a more secure method for encryption, but the basics are there. The documentation makes it far from clear what setup is necessary, so it would be good to explore that, and understand the whole system more fully. This is a reasonable starting point in any case. Notes contain a few details.

--- TLS/server.hs
+++ TLS/server.hs
@@ -1,11 +1,17 @@
 -- Basic TCP server - to develop for TLS connection
+{-# LANGUAGE OverloadedStrings #-}
+
 
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import qualified Data.ByteString as B
-import Data.ByteString.Char8 (pack)
-import Network.Socket.ByteString
+import Data.String
 
 import Network.TLS
+import Control.Monad.IO.Class
+import System.IO
+import Data.X509
+import Data.Default.Class
+import Network.TLS.Extra.Cipher
 
 main :: IO ()
 main = withSocketsDo $ do
@@ -17,17 +23,35 @@ main = withSocketsDo $ do
   -- If we can, serve at this address
   if null addresses then error "Can't create server at specified address" else bind server (head addresses)
   listen server 5
-  
+
+  tlsParams <- makeServerParams
+  putStrLn $ show tlsParams
+
   -- This loops forever, dispatching a basic message to anything that
   -- connectes. One can connect with a web browser for instance.
   forever $ do
     -- Get the next connection, and show the details
     (connection, client) <- accept server
     putStrLn $ "Got connection from " ++ show client
-    -- Receive a message
-    (message,_) <- recvFrom connection 4096
+
+    -- Enable encryption on the connections
+    context <- contextNew connection tlsParams
+    handshake context
+    message <- recvData context
     putStrLn $ "Received: " ++ show message
     -- Send one back
-    sendTo connection (pack "Hello from Haskell!") client
+    sendData context (fromString "Hello from secure Haskell!")
+    bye context
+    contextClose context
+    
     close connection
   where forever a = a >> forever a
+
+makeServerParams :: (MonadIO m) => m ServerParams
+makeServerParams = do
+  attempted <- liftIO $ credentialLoadX509 "cert.pem" "key.pem"
+  -- Now pattern match credential, certificate chain and private key (ignored)
+  let (Right cred@(cc, _)) = attempted
+      CertificateChain listOfSignedCerts = cc
+  return $ ServerParams False listOfSignedCerts Nothing (def {sharedCredentials = Credentials [cred]}) def (def {supportedCiphers = ciphersuite_all})
+  
