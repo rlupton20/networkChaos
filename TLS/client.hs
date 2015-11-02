94887cd9b9a6cbbb5733dab5e5c3e3f45e4125bb
Richard <richard.lupton@gmail.com>
2015-11-02 22:08:07 +0000

    Have a working basic TLS client. Much learned about creating TLS connections, certificates etc. Works with the server. Food for thought for later design.

--- TLS/client.hs
+++ TLS/client.hs
@@ -1,9 +1,16 @@
 -- A basic TCP client - to develop for TLS connection
+{-# LANGUAGE OverloadedStrings #-}
 
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import qualified Data.ByteString as B
-import Data.ByteString.Char8 (pack)
-import Network.Socket.ByteString
+import Control.Monad.IO.Class
+
+import Network.TLS
+import Network.TLS.Extra.Cipher
+import Data.String
+import Data.X509
+import Data.X509.CertificateStore
+import Data.X509.File
 
 main :: IO ()
 main = withSocketsDo $ do
@@ -15,12 +22,36 @@ main = withSocketsDo $ do
   serverAddrs <- getAddrInfo Nothing (Just $ "localhost") (Just $ show 12345)
   let addr = addrAddress $ head serverAddrs
 
-  -- Connect to the server and send a small message
+  -- Connect to the server and send a small encrypted message
   connect client addr
-  sendTo client (pack "GET") addr
 
-  -- Wait for a reply, and print it to screen
-  (reply,_) <- recvFrom client 4096
+  -- Using self-signed certificates requires clients to know
+  -- such certificates are trusted. We load them and make our
+  -- client aware
+  caCerts <- loadCACerts
+
+  -- This should be possible to tidy up a bit (lenses?)
+  let tlsParams' = defaultParamsClient "localhost" B.empty
+      sup = clientSupported tlsParams'
+      newSupported = sup { supportedCiphers = ciphersuite_all }
+      shar = clientShared tlsParams'
+      newShared = shar { sharedCAStore = caCerts }
+      tlsParams = tlsParams' { clientSupported = newSupported, clientShared = newShared, clientUseServerNameIndication = False }
+
+  -- Establish encrypted connection
+  context <- contextNew client tlsParams
+  handshake context
+  -- Send an encrypted message
+  sendData context (fromString "GET")
+
+  -- Receive an encrypted message
+  reply <- recvData context
   putStrLn $ show reply
 
   close client
+
+loadCACerts :: (MonadIO m) => m CertificateStore
+loadCACerts = liftIO $ do
+  signedCerts <- readSignedObject "cert.pem"
+  let certStore = makeCertificateStore signedCerts
+  return certStore
