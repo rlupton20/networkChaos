3455b85c8d2ce7990c2ab7f728a2dfbee2a5a0a3
Richard <richard.lupton@gmail.com>
2015-11-24 13:06:39 +0000

    Wrote a fast ethernet header stripper which avoids parsing, by just dropping the header. Seems to agree with the parsing version. This may not be entirely robust, depending on the exact format of the ethernet headers, so this may need a bit of development later.

--- src/Routing/PacketParsing/Ether.hs
+++ src/Routing/PacketParsing/Ether.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Routing.PacketParsing.Ether
 ( stripEther
 , makeEtherStripper ) where
@@ -13,8 +14,18 @@ import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TQueue
 
-stripEther :: B.ByteString -> Maybe B.ByteString
-stripEther = fmap (B.pack . outBytes . doUnparse) . tryRemoveEther
+-- The ethernet header is 14 bytes long so just drop it to get contents
+-- WARNING: Different header types may cause problems, so if this
+-- is kept later, it may need modifying. One advantage is its content
+-- agnostic, so will deal with anything coming via ethernet.
+-- See: tryRemoveEther, below.
+stripEther :: B.ByteString -> B.ByteString
+stripEther = B.drop 14
+
+-- This is the old version of stripEther, which actually
+-- parses the packet and extracts its contents.
+stripEther' :: B.ByteString -> Maybe B.ByteString
+stripEther' = fmap (B.pack . outBytes . doUnparse) . tryRemoveEther
 
 makeEtherStripper :: (B.ByteString -> IO ()) -> IO (TQueue B.ByteString)
 makeEtherStripper outfeed = do
@@ -25,14 +36,16 @@ makeEtherStripper outfeed = do
   where
     loop infeed = do
       pck <- atomically $ readTQueue infeed
-      case stripEther pck of
-        (Just spck) -> outfeed spck
-        Nothing -> return ()
+      let !spck = stripEther pck
+      outfeed spck
       loop infeed
   
 restruct :: B.ByteString -> InPacket
 restruct pk = toInPack $ listArray (0, B.length pk - 1) (B.unpack pk)
 
+-- This function (tries to) parse an ethernet packet, and
+-- returns an internal IP packet if there is one.
+-- Question: could this miss some packets? e.g. if not IP?
 tryRemoveEther :: B.ByteString -> Maybe (Net.IPv4.Packet InPacket)
 tryRemoveEther = fmap (Net.Ethernet.content) . doParse . restruct
 
