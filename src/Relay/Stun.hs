807732b0204dd8a9d7bcc7bdeb5482af54be02d3
Richard <richard.lupton@gmail.com>
2015-12-14 13:07:38 +0000

    Developing STUN function. Beginning to decode messages using Network.Stun, and keeping the socket used. Not particularly robust at the moment.

--- src/Relay/Stun.hs
+++ src/Relay/Stun.hs
@@ -2,13 +2,22 @@ module Relay.Stun
 ( ) where
 
 import Relay.Connection
+import Network.Socket
 import Network.Stun
+import Network.Stun.Internal
 
 stunServer = "stun.ekiga.net"
 
-stun :: IO ()
+-- stun returns a socket with its external address
+-- This isn't robust yet...
+stun :: IO (Socket, SockAddr)
 stun = do
   brq <- bindRequest
   stunAddr <- resolveAddr stunServer 3478
-  stunr <- findMappedAddress stunAddr 0 [] --stunRequest' stunAddr 0 [] brq
-  putStrLn $ show stunr
+  -- stunr <- findMappedAddress stunAddr 0 []
+  Right (msg, sock) <- stunRequest' stunAddr 0 [] brq
+  let ma = (findAttribute ( messageAttributes msg ) :: Either AttributeError [MappedAddress])
+      ext = fmap (fmap unMA) ma
+  case ext of
+       Right [ad] -> return (sock, ad)
+       _ -> error "Couldn't STUN."
