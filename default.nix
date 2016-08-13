0f2d46eeb302f53c4d45ef6600cdd6e7df60ba22
Richard Lupton <richard.lupton@gmail.com>
2016-08-13 13:46:32 +0100

    Created a basic nix file.

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs; with pkgs.haskellPackages;
+mkDerivation {
+  pname = "vanguard";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [ cabal-install stack happy ghc-mod hindent hlint hasktags stylish-haskell ];
+  executableHaskellDepends = [
+    async base bytestring cereal connection containers
+    data-default-class http-client http-client-tls mtl network
+    network-house stm stunclient tls transformers unbounded-delays unix
+    unix-bytestring yaml
+  ];
+  testHaskellDepends = [
+    async base bytestring connection containers data-default-class
+    http-client http-client-tls HUnit mtl QuickCheck stm test-framework
+    test-framework-hunit test-framework-quickcheck2 time tls
+    transformers yaml
+  ];
+  homepage = "https://github.com/rlupton20/networkChaos";
+  description = "A peer-to-peer VPN";
+  license = stdenv.lib.licenses.gpl3;
+}
