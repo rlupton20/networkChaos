d07c4be8d880e4020dbacbd4fca52981cd6eb56c
Richard Lupton <richard.lupton@gmail.com>
2016-08-14 20:42:29 +0100

    Basic communication with bootstrapping node.

--- app/Oracle/API/Internal.hs
+++ app/Oracle/API/Internal.hs
@@ -1,17 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Oracle.API.Internal where
 
+import           Control.Applicative       ((<$>))
+
 import qualified Data.ByteString           as B
 import           Data.String               (fromString)
 import qualified Network.HTTP.Client       as H
 import           Network.HTTP.Client.TLS   (mkManagerSettings)
 import           Network.Connection        (TLSSettings(..))
+  
 import           Network.TLS               (ClientParams(..), defaultParamsClient
                                            , Supported(..), Version(TLS12)
+                                           , Shared(..)
                                            , HashAlgorithm(HashSHA512), SignatureAlgorithm(SignatureRSA) )
 import           Network.TLS.Extra.Cipher  (ciphersuite_strong)
+import           Data.X509.File            (readSignedObject, readKeyFile)
+import           Data.X509.CertificateStore (makeCertificateStore)
 import           Data.Default.Class        (def) 
-  
+
 import qualified Config as C
 
 
@@ -27,16 +33,16 @@ makeOracle config = return $ Oracle (C.address config) (C.oracleCert config)
 
 get :: String -> Oracle -> IO String
 get path oracle = do
+  certs <- makeCertificateStore <$> readSignedObject (cert oracle)
   let supported = def { supportedVersions = [TLS12]
                       , supportedCiphers = ciphersuite_strong
                       , supportedHashSignatures = [ (HashSHA512, SignatureRSA) ] }
+      shared = def { sharedCAStore = certs }
       templateParams = defaultParamsClient "vanguard-node" ""
-      clientParams = templateParams { clientSupported = supported }
+      clientParams = templateParams { clientSupported = supported
+                                    , clientShared = shared }
       tls = TLSSettings clientParams
 
---  putStrLn $ show supported
-  putStrLn $ show clientParams
-  
   manager <- H.newManager $ mkManagerSettings tls Nothing
 
   request <- H.parseUrl $ address oracle ++ path
