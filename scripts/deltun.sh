d472c70b5e9cda825e9cb18ca92309790edaadc0
Richard Lupton <richard.lupton@gmail.com>
2017-02-18 23:46:46 +0000

    Tidy files
    Removing unecessary scripts. Removing Makefile (never use it).

--- scripts/deltun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# $1 is the name of the tunnel interface to delete
-ip tuntap del dev $1 mode tun
--- scripts/makeDummy.sh
+++
@@ -1,7 +0,0 @@
-#!\bin\bash
-
-modprobe dummy
-ip link add rvl0 type dummy
-ifconfig rvl0 up
-
-route add -net 10.0.0.0 netmask 255.255.255.0 dev rvl0
